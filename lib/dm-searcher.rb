473f97bd99d90c9fc7700fab04fe81ea38533a02
Tower He <towerhe@gmail.com>
2012-07-10 23:38:56 +0800

    Initial Commit

---
+++ lib/dm-searcher.rb
@@ -0,0 +1,7 @@
+require "dm-searcher/version"
+
+module Dm
+  module Searcher
+    # Your code goes here...
+  end
+end
---
+++ lib/dm-searcher/version.rb
@@ -0,0 +1,5 @@
+module Dm
+  module Searcher
+    VERSION = "0.0.1"
+  end
+end
